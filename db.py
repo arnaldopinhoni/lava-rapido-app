3ad7b4d681d124f60ae421d476011cdf1d87a56b
arnaldopinhoni <arnaldo.pinhoni@gmail.com>
2025-12-11 21:05:06 -0300

    use supabase pooler connection

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ def get_connection():
         port=os.getenv("DB_PORT"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        dbname=os.getenv("DB_NAME")
+        dbname=os.getenv("DB_NAME"),
         sslmode="require"
     )
 
